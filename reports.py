ee2a1265cdf9bfa2a7f69cbd537682658ef7cd66
Finn <finn@finn.io>
2016-01-29 10:48:53 -0800

    all() rather than find()

--- reports.py
+++ reports.py
@@ -8,18 +8,17 @@ app.config.from_pyfile('default-config.py')
 app.config.from_pyfile('config.py', silent=True)
 
 db = dataset.connect(app.config['DATABASE'])
-reports = db['reports']
 
 
 @app.route("/")
 def hello():
-    return render_template('index.html', reports=reports.find())
+    return render_template('index.html', reports=db['reports'].all())
 
 
 @app.route("/csp", methods=["POST"])
 def csp():
     row = dict(json.loads(request.data)['csp-report'])
-    reports.insert(row)
+    db['reports'].insert(row)
     return jsonify({"success": True})
 
 if __name__ == "__main__":
