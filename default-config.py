f2296d9f5084a8f6a4372c6913909b8e3ee1ca5e
Finn <finn@finn.io>
2016-01-29 10:31:22 -0800

    Log to a db

---
+++ default-config.py
@@ -0,0 +1,1 @@
+DATABASE = csp.db
--- reports.py
+++ reports.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python
-from flask import Flask, request, jsonify
-import logging
-from logging.handlers import RotatingFileHandler
 import json
-app = Flask(__name__)
+import dataset
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+app.config.from_pyfile('default-config.py')
 app.config.from_pyfile('config.py', silent=True)
 
-handler = RotatingFileHandler('csp.log', maxBytes=10000, backupCount=1)
-handler.setLevel(logging.DEBUG)
-app.logger.addHandler(handler)
+db = dataset.connect(app.config['DATABASE'])
+reports = db['reports']
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return jsonify(reports.find())
 
 
 @app.route("/csp", methods=["POST"])
 def csp():
-    data = json.loads(request.data)
-    app.logger.info(json.dumps(data))
-    return jsonify({"success": True, "data": data})
+    row = dict(json.loads(request.data)['csp-report'])
+    reports.insert(row)
+    return jsonify({"success": True})
 
 if __name__ == "__main__":
     app.run()
