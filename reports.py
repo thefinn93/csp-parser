e88a5fef8f19e2448e789e1ffff201a87d615126
Finn <finn@finn.io>
2016-01-29 12:05:15 -0800

    Break the report down by domain

--- reports.py
+++ reports.py
@@ -12,29 +12,30 @@ db = dataset.connect(app.config['DATABASE'])
 
 
 @app.route("/")
-def hello():
-    return render_template('index.html', reports=db['reports'].all())
+def index():
+    return render_template('index.html', domains=db['reports'].distinct('domain'))
+
+
+@app.route("/<domain>")
+def domain_report(domain):
+    return render_template('report.html', reports=db['reports'].find(domain=domain))
 
 
 @app.route("/csp", methods=["POST"])
 def csp():
-    try:
-        data = json.loads(request.data)['csp-report']
-        row = dict()
-        for key in data:
-            row[key.replace("-", "_")] = data[key]
-        if "document-uri" in data:
-            url = urlparse(data['document-uri'])
-            if url.scheme != "view-source":
-                row['domain'] = url.netloc
-            else:
-                url = urlparse(url.path)
-                row['domain'] = url.netloc
-        db['reports'].insert(row)
-        return jsonify({"success": True})
-    except KeyboardInterrupt:
-        return(jsonify({"success": False, "data": request.data}))
-
+    data = json.loads(request.data)['csp-report']
+    row = dict()
+    for key in data:
+        row[key.replace("-", "_")] = data[key]
+    if "document-uri" in data:
+        url = urlparse(data['document-uri'])
+        if url.scheme != "view-source":
+            row['domain'] = url.netloc
+        else:
+            url = urlparse(url.path)
+            row['domain'] = url.netloc
+    db['reports'].insert(row)
+    return jsonify({"success": True})
 
 if __name__ == "__main__":
     app.run()
